2bc2a7d43726d4d0e131349498526e35bb5135cc
matteus isaque <matteus.isaque28@gmail.com>
2024-05-31 11:43:28 -0300

    alt

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import { WhatsappLogo } from "phosphor-react";
-import Img1 from "./img/0367968c-3cfd-41c2-a8d8-9e849b9b9f42.jpg";
-import Img2 from "./img/1009d456-2334-4e7a-900a-8c96a7abbd22-1.jpg";
-import Img3 from "./img/33864f4e-a52a-4d2a-acd8-3d03892b24fa.jpg";
-import Img4 from "./img/5dccc955-5996-4134-8eed-113d16305298.jpg";
-import Img5 from "./img/5f41c040-140c-4368-bfeb-33e3a6982de6.jpg";
-import Img6 from "./img/6a3a5d82-3280-45c6-a2f4-d835db6f7dae.jpg";
-import Img7 from "./img/8b8391d8-6883-4249-a2a2-70ed7095e5c2.jpg";
-import Img8 from "./img/IMG_2571.jpg";
-import Img9 from "./img/IMG_3428.jpg";
-import Img10 from "./img/IMG_7859.jpg";
-import Img11 from "./img/IMG_8267.jpg";
-import Img12 from "./img/a3aced40-6d3b-4b0a-9366-76610134c86f.jpg";
+import Img from "./img/ALFAIATARIA gotino.jpg";
+import Img2 from "./img/ALFAIATARIA henri.jpg";
+import Img7 from "./img/client.jpg";
+import Img6 from "./img/doutor.jpg";
+import Img3 from "./img/IMG_2780.jpg";
+import Img4 from "./img/IMG_9348 politico.jpg";
+import Img5 from "./img/WhatsApp Image 2024-05-16 at 19.26.03.jpeg.jpg";
 
 export function App() {
   return (
     <main>
-      <div id="main-background" className="min-h-screen h-full w-full mobile:min-h-[90vh]">
+      <div
+        id="main-background"
+        className="min-h-screen h-full w-full mobile:min-h-[90vh]"
+      >
         <div className="flex flex-col justify-center items-start h-screen ml-[10%] max-w-[750px] mobile:h-[80vh] mobile:mx-[5%]">
           <h1 className="text-7xl font-semibold text-start text-white mobile:text-5xl mobile:text-center mobile:leading-[55px]">
             Roupas sob medida para pessoas modernas
           </h1>
-          <button className="border rounded-xl text-white flex justify-center items-center gap-2 text-xl px-10 py-2 mt-6 bg-[#199347] font-medium hover:bg-[#116631] transition-all mobile:w-full"><WhatsappLogo size={24} /> Falar com atendente</button>
+          <button className="border rounded-xl text-white flex justify-center items-center gap-2 text-xl px-10 py-2 mt-6 bg-[#199347] font-medium hover:bg-[#116631] transition-all mobile:w-full">
+            <WhatsappLogo size={24} /> Falar com atendente
+          </button>
         </div>
       </div>
 
@@ -43,7 +43,7 @@ export function App() {
         </div>
         <div className="grid grid-cols-4 gap-4 mobile:grid-cols-2">
           <div>
-            <img src={Img1} />
+            <img src={Img} />
           </div>
           <div>
             <img src={Img2} />
@@ -62,21 +62,6 @@ export function App() {
           </div>
           <div>
             <img src={Img7} />
-          </div>
-          <div>
-            <img src={Img8} />
-          </div>
-          <div>
-            <img src={Img9} />
-          </div>
-          <div>
-            <img src={Img10} />
-          </div>
-          <div>
-            <img src={Img11} />
-          </div>
-          <div>
-            <img src={Img12} />
           </div>
         </div>
       </div>
